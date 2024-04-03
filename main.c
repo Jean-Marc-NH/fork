0e39b83cc623e767dd819f1dd31417d03ddf5cf9
Jean Marc Nadeau Huamani <101827583+Jean-Marc-NH@users.noreply.github.com>
2024-04-03 15:35:42 -0500

    Update main.c

--- main.c
+++ main.c
@@ -8,15 +8,15 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-int CONT=0;
+int vueltas=0;
 int signalON;
 int main(void)
 {
         int     pipe1[2],pipe2[2], nbytes;
         pid_t   childpid;
         char    readbuffer[80];
-        int c = 99;
-        sprintf(readbuffer, "%d\n ",c); 
+        int size = 99;
+        sprintf(readbuffer, "%d\n ",size); 
         pipe(pipe1);
         pipe(pipe2);
 
@@ -31,34 +31,26 @@ int main(void)
  
         
         
-        if(childpid == 0) // HIJO
+        if(childpid == 0)
         {
                 for(;;)
                 {
-                    printf("Hijo:\n"); 
-                    close(pipe1[1]);
-                    nbytes = read(pipe1[0], readbuffer, sizeof(readbuffer));               
-                    printf("Valor recivido: %s ", readbuffer);
+                    printf("[-] Hijo:\n"); 
+                    close(pipe1[1]); // 1 escritura
+                    nbytes = read(pipe1[0], readbuffer, sizeof(readbuffer));  // 0 lectura              
+                    printf("Valor recibido: %s", readbuffer);
 
                     int num = atoi(readbuffer);
                     num = num +3;
                     printf("+ 3 = %d\n",num);
-                    // calculo +3
-
-                     CONT = CONT + 1;
-                        if (CONT > 10) 
-                        { 
-                            exit(0);
-                        }
-
-
-                    sprintf(readbuffer, "%d\n ",num);  
-
-                   
-
-
+                    sprintf(readbuffer, "%d",num);  
                     close(pipe2[0]);
                     write(pipe2[1], readbuffer, (strlen(readbuffer)));
+                    vueltas = vueltas + 1;
+                    if (vueltas > 10) 
+                    { 
+                        exit(0);
+                    }
                     }
                 exit(0);
         }
@@ -67,19 +59,19 @@ int main(void)
             
 
                     for(;;){
-                        printf("Padre: \n");
-                        close(pipe1[0]);
-                        write(pipe1[1], readbuffer, (strlen(readbuffer)+1));
+                        printf("[+] Padre: \n");
+                        close(pipe1[0]); // 0 lectura
+                        write(pipe1[1], readbuffer, (strlen(readbuffer)+1)); // 1 escritura
 
 
                         close(pipe2[1]);                       
                         nbytes = read(pipe2[0], readbuffer, sizeof(readbuffer));               
-                        printf("Valor recivido: %s ", readbuffer);
+                        printf("Valor recibido: %s", readbuffer);
                         int num = atoi(readbuffer);
                         num = num +7;
-                        printf("+ 7 =  %d\n",num);
+                        printf("+ 7 = %d\n",num);
 
-                        sprintf(readbuffer, "%d\n ",num); 
+                        sprintf(readbuffer, "%d ",num); 
                     }
                 
             
